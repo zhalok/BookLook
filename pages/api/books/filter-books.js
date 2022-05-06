c462d654839fa65214e399ec3d94c368904efa35
kaifatab <kaifatab>
2022-05-06 10:52:32 +0600

    update

--- pages/api/books/filter-books.js
+++ pages/api/books/filter-books.js
@@ -2,12 +2,13 @@ const mysqlClient = require("../../../utils/database_connection");
 
 export default async function handler(req, res) {
   const { authors, publications, catagories, courses } = req.body;
+  console.log(catagories);
   const queryString1 =
-    "select id from books where author in (?) and publication in (?)";
+    "select id from books where author in (?) and publication in (?) and id in (select bookId from books_catagories where catagory in (?) and bookId in (select bookId from book_courses where course in (?)) )  ";
   const promise1 = new Promise((resolve, reject) => {
     mysqlClient.query(
       queryString1,
-      [authors, publications],
+      [authors, publications, catagories, courses],
       (err, rows, fields) => {
         if (err) {
           reject(err);
