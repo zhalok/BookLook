bf44a30e2ac3f6be158b91603ad3373d3d6c4bea
zhalok <zhalokrahman007@gmail.com>
2022-05-10 11:05:27 +0600

    getting token using api

---
+++ pages/api/auth/request-token.js
@@ -0,0 +1,22 @@
+const query_string = require("querystring");
+const { google } = require("googleapis");
+const fs = require("fs");
+export default function handler(req, res) {
+  const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+  let credentials = fs.readFileSync(
+    "/home/zhalok/Desktop/BookLook/credentials.json"
+  );
+  credentials = JSON.parse(credentials);
+  const { client_secret, client_id, redirect_uris } = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  console.log(redirect_uris);
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  res.redirect(authUrl);
+}
