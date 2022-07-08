cad205d8fa5f6126a9bd1feb4a9a910ff0a4873f
zhalok <zhalok>
2022-07-08 13:21:44 +0600

    user-recommendation-update

---
+++ pages/api/books/recommend.js
@@ -0,0 +1,27 @@
+const mysqlClient = require("../../../utils/database_connection");
+export default async function handler(req, res) {
+  try {
+    const { bookId, userId } = req.query;
+    await new Promise((resolve, reject) => {
+      const queryString =
+        "update books set recommendations = recommendations +1 where id = ? ";
+      mysqlClient.query(queryString, [bookId], (err, row) => {
+        if (err) reject(err);
+        else resolve(row);
+      });
+    });
+    await new Promise((resolve, reject) => {
+      mysqlClient.query(
+        "insert into user_recommendation (userId,bookId) values (?)",
+        [[bookId, userId]],
+        (err, rows) => {
+          if (err) reject(err);
+          else resolve(rows);
+        }
+      );
+    });
+    res.status(200).json({ message: "Recommendation Updated" });
+  } catch (e) {
+    res.json(e);
+  }
+}
---
+++ queries/CreateTables/create-user-recommendation-table.sql
@@ -0,0 +1,7 @@
+-- use bqlz2crijfharfe0jjuo;
+use BookLook;
+
+create table user_recommendation(
+    userId int not null,
+    bookId int not null
+)
