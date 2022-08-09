ab46e6e56e7fd6fd2a6f0033f7ba9b8ca2f52b68
zhalok <zhalok>
2022-08-09 19:09:13 +0600

    working on user deletion

--- pages/api/user/delete/[id].js
+++ pages/api/user/delete/[id].js
@@ -1,4 +1,7 @@
+import { deleteObject } from "firebase/storage";
+
 const mysqlClient = require("../../../../utils/database_connection");
+const firebaseStorage = require("../../../../utils/firebaseConnection");
 export default async function handler(req, res) {
   if (req.method != "DELETE") {
     res.status(400).json({ message: "Bad request" });
@@ -6,6 +9,29 @@ export default async function handler(req, res) {
   }
   const { id } = req.query;
   try {
+    const data = await new Promise((resolve, reject) => {
+      const queryString = "select id from books where uploader=?";
+      mysqlClient.query(queryString, [id], (err, rows) => {
+        if (err) reject(err);
+        else resolve(rows);
+      });
+    });
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      console.log(data);
+      const fileRef = ref(firebaseStorage, `${data[0].id}`);
+      await deleteObject(fileRef);
+    }
+    await new Promise((resolve, reject) => {
+      mysqlClient.query(
+        "delete from books where uploader=?",
+        [id],
+        (err, rows) => {
+          if (err) reject(err);
+          else resolve(rows);
+        }
+      );
+    });
     await new Promise((resolve, reject) => {
       const queryString = "delete from users where id=?";
       mysqlClient.query(queryString, [id], (err, rows) => {
@@ -13,6 +39,7 @@ export default async function handler(req, res) {
         else resolve(rows);
       });
     });
+
     res.json({ message: "User deleted" });
   } catch (e) {
     res.json(e);
